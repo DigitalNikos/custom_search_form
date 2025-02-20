8f28d5ee211617574a5b84dcf53cdeb60141a10d
Nikos Lithoxopoulos <lithoxopoulosnikos@gmail.com>
2025-02-20 11:17:47 +0100

    Add folders/Structure

---
+++ my-real-esate-search.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Real Estate Search Form
+ * Plugin URI:  https://example.com
+ * Description: A plugin to display custom real estate search forms with Rent/Buy toggle and property filtering.
+ * Version:     1.0
+ * Author:      Lithoxopoulos Nikolaos
+ * Author URI:  https://example.com
+ * License:     GPL2
+ * Text Domain: my-real-estate-search
+ */
+
+ if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly.
+}
